df098362bcc0639e574b854751922f4b8281fe3c
Neotheweirdone <45073167+Neotheweirdone@users.noreply.github.com>
2019-02-17 21:50:40 +0530

    Updating with redux modules

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,29 @@
 import React,{Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import Searchbox from '../components/Searchbox';
 import './App.css';
+import ErrorBoundary from '../components/ErrorBoundary';
 import Scroll from '../components/Scroll';
+import  {searchRobots} from '../reducers';
+import {setSearchField} from '../actions'
 
+const mapStateToProps=state=>{
+	return{
+		searchField:state.searchRobots.searchField 
+	}
+}
+
+const mapDispatchToProps=(dispatch)=>{
+	return{
+	onSearchChange: (event)=>dispatch(setSearchField(event.target.value))
+}
+}
 class App extends Component{
 	constructor(){
 		super()
 		this.state={
-			robots:[],
-			searchfield:''
+			robots:[]
 		}
 	}
 	componentDidMount(){
@@ -17,13 +31,14 @@ class App extends Component{
 		.then(response=>response.json())
 		.then(users=>this.setState({robots:users}));
 	}
-	onSearchChange=(event)=>{//use arrows when making methods in React
-			this.setState({searchfield:event.target.value})//event.target.value gives event of the search job
-	}
+	//use arrows when making methods in React
+			//event.target.value gives event of the search job
+	
 	render(){
-		const {robots,searchfield}=this.state;//destructuring one time declaration
+		const {robots}=this.state;//destructuring one time declaration
+		const {searchField,onSearchChange}=this.props;
 		const filteredRobots=robots.filter(robot=>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 			})
 		if(!robots.length){
 			return <h1 className='tc'>Loading</h1>
@@ -33,14 +48,15 @@ class App extends Component{
 	return(
 		<div className='tc'>
 			<h1 className='f1'>Robofriends</h1>	
-			<Searchbox searchChange={this.onSearchChange}/>
+			<Searchbox searchChange={onSearchChange}/>
 			<Scroll>
-			<CardList robots={filteredRobots}/>
+			<ErrorBoundary>
+				<CardList robots={filteredRobots}/>
+			</ErrorBoundary>
 			</Scroll>
 		</div>
-		
-	);
+		);
 }
 }
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
